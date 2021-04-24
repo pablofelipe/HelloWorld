3bde9533ab189f830a1da4168c1a2f3df93bd3f8
pablofelipe <pablofelipe@gmail.com>
2021-04-24 16:54:39 -0300

    Add files via upload

---
+++ src/main/java/fiap/evaluation/HelloWorld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package fiap.evaluation.HelloWorld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/HelloWorldController")
+public class HelloWorldController {
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	public String hello() {
+		
+		return "Hello World!";
+	}
+}
